cdf17de84b61ffbbd70e62b74a8d99c8311744bc
matheus <matheuslima20111997@gmail.com>
2020-06-27 11:29:39 -0300

    exercicio

---
+++ Modulo_10_exercicio/script.js
@@ -0,0 +1,16 @@
+let objCalculadora={
+    
+    somar:function(num1,num2){
+        return num1+num2
+    },
+    subtrair:function(num1,num2){
+        return num1-num2
+    },
+    multiplicar:function(num1,num2){
+        return num1*num2
+    },
+    dividir:function(num1,num2){
+        return num1/num2
+    }
+
+}
